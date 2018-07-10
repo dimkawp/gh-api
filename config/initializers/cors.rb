d41e9b3d5f520f30d5f7db2aac219b0d2a5eb976
dimkawp <dimkawp@gmail.com>
2018-07-10 14:38:25 +0300

    add corse fore 'wok'

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://gh-homework-13.herokuapp.com', 'https://my-self-prod.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001'
+    origins 'https://gh-homework-13.herokuapp.com', 'https://my-self-prod.herokuapp.com', 'http://localhost:3000', 'http://localhost:3001', 'https://worldofkeg.com.ua', 'http://worldofkeg.com.ua'
     resource '*',
              headers: :any,
              methods: %i(get post put patch delete options head)
